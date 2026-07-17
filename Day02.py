77b9044020811ec2c8914cbc061c29e3ab58bdf4
buildwithlou <buildwithlou@gmail.com>
2026-07-17 15:44:45 -0400

    Day 02 - Variables

---
+++ Day02.py
@@ -0,0 +1,12 @@
+# Variables - Day 02
+skills = ["HTML", "CSS", "JS", "React", "Python"]  # List
+personalInfo = {  # Dictionary
+    "firstname": "Lourdes",
+    "lastname": "Pampa",
+    "country": "Peru",
+    "city": "Lima",
+}
+main = (1, 2), ("a", "b"), ("Lourdes", "Willisito")  # Tuple
+
+print(type(skills))
+print(type(main))
