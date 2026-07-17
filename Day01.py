c9e3170478d1221370a431bc5c054189edbb586b
buildwithlou <buildwithlou@gmail.com>
2026-07-17 15:29:56 -0400

    Day 01 - Introduction

---
+++ Day01.py
@@ -0,0 +1,11 @@
+# Introduction - Day 01
+print("Hello World!")
+
+print(3**2)  # exponential (**)
+print(3 % 2)  # modulus (%)
+print(3 // 2)  # floor division operator (//)
+
+print(type(1 + 3))  # Complex
+print(type([1, 2, 3, 4]))  # List
+print(type({"name": "Lourdes"}))  # Dictionary
+print(type({27.0, 15.0, 8.9}))  # Tuple
