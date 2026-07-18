351b7b7f15523fd94e5473f8d489de04550ce2b1
buildwithlou <buildwithlou@gmail.com>
2026-07-18 15:17:08 -0400

    Day 02 - Exercises level 02 (4-11)

--- Day_02/variables.py
+++ Day_02/variables.py
@@ -14,14 +14,17 @@ print(type(age))
 print(type(height))
 print(type(status))
 
+print(50 * "-")
 print(len(firstName))
 print(len(lastName))
 print(len(fullName))
 print(len(country))
+print(len(status))
 # print(len(age)) #int has no length
 # print(len(height)) #float has no length
-print(len(status))
 
+# Compare the length of the first Name and last Name
+print(50 * "-")
 lenfirstName = len(firstName)
 lenlastName = len(lastName)
 print(f"{firstName} has {lenfirstName} letters")
@@ -33,3 +36,22 @@ elif lenlastName > lenfirstName:
     print(f"{lastName} is longer")
 else:
     print("They're the same length")
+
+# Declaring num_one and num_two
+print(50 * "-")
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(f"Total: {total}")
+diff = num_one - num_two
+print(f"Difference: {diff}")
+product = num_two * num_one
+print(f"Product: {product}")
+division = num_one / num_two
+print(f"Division: {division}")
+remainder = num_one % num_two
+print(f"Remainder: {remainder}")
+exp = num_one**num_two
+print(f"Exponent: {exp}")
+floor_division = num_one // num_two
+print(f"Floor Division: {floor_division}")
