d14174b48998f70762d122e1d2dd3d39bb821e35
buildwithlou <buildwithlou@gmail.com>
2026-07-18 15:00:45 -0400

    Making some changes

---
+++ Day_02/variables.py
@@ -0,0 +1,28 @@
+# Exercises - Level 1
+firstName = "Lourdes"
+lastName = "Pampa"
+fullName = "Lourdes Pampa"
+country = "Peru"
+age, height, status = 25, 5.01, "Married"
+
+# Exercises - Level 2
+print(type(firstName))
+print(type(lastName))
+print(type(fullName))
+print(type(country))
+print(type(age))
+print(type(height))
+print(type(status))
+
+print(len(firstName))
+print(len(lastName))
+print(len(fullName))
+print(len(country))
+# print(len(age)) #int has no length
+# print(len(height)) #float has no length
+print(len(status))
+
+lenfirstName = len(firstName)
+lenlastName = len(lastName)
+print(f"{firstName} has {lenfirstName} letters")
+print(f"{lastName} has {lenlastName} letters")
