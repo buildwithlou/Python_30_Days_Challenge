5b4b09983ac7336178b35841a9542808863b81dc
buildwithlou <buildwithlou@gmail.com>
2026-07-19 13:00:36 -0400

    Day 03 - Exercises level 02 (1-4)

---
+++ Day_03/comparisonOperators.py
@@ -0,0 +1,10 @@
+# Variables
+age = 25
+height = 5.01
+Cnumber = 1 + 3
+
+# User entering the base and height of the triangle and calculate the area of this triangle
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+area = 0.5 * base * height
+print(f"The area of the triangle is {area}")
