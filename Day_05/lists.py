a70f469fd42d2112aa54262cdf4710dbc81e879a
buildwithlou <buildwithlou@gmail.com>
2026-07-25 21:06:58 -0400

    Day 04 - Exercises (3)

--- Day_05/lists.py
+++ Day_05/lists.py
@@ -3,3 +3,5 @@
 empty_list = []
 # Declaring a list with more than 5 items
 web_techs = ["HTML", "CSS", "JavaScript", "React", "Python", "Redux"]
+# Finding the length of the list that you just created
+print("Length of the list: ", len(web_techs))
