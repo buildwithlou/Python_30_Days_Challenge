1fddcde8e9a4477c6361b5220eb691c8a740ea36
buildwithlou <buildwithlou@gmail.com>
2026-07-25 21:12:42 -0400

    Day 04 - Exercises (4)

--- Day_05/lists.py
+++ Day_05/lists.py
@@ -5,3 +5,10 @@ empty_list = []
 web_techs = ["HTML", "CSS", "JavaScript", "React", "Python", "Redux"]
 # Finding the length of the list that you just created
 print("Length of the list: ", len(web_techs))
+# Finding the first item, middle item and last item
+print(
+    "First, Middle and Last item: ",
+    web_techs[0],
+    web_techs[len(web_techs) // 2],
+    web_techs[-2],
+)
