c6885acb9dcd7615e55c1721d04178d826dc4d90
buildwithlou <buildwithlou@gmail.com>
2026-07-28 18:13:23 -0400

    Day 06 - Exercises (2)

--- Day_06/tuples.py
+++ Day_06/tuples.py
@@ -1,3 +1,5 @@
 # Exercises - Day 06
 # Creating an empty tuple
 tuple_example = tuple()
+# Creating a tuple with my family names
+siblings = ("Max", "Ricardo", "Nina", "Felipe", "Griselle", "Soledad", "Jhojan", "Luis")
