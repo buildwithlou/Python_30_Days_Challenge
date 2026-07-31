6517f5250e5152109fd73a36c437fbbb72c11d25
buildwithlou <buildwithlou@gmail.com>
2026-07-30 23:14:33 -0400

    Day 07 - Exercises (3)

--- Day_07/sets.py
+++ Day_07/sets.py
@@ -6,6 +6,9 @@ B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
 # Finding the length of the it_companies
 print("The length of the it_companies set is: ", len(it_companies))
-# Adding a value into the sets
+# Adding a value into the set
 it_companies.add("Twitter")
+print(it_companies)
+# Adding multiple values into the set
+it_companies.update(["Cisco", "Intel", "Dropbox"])
 print(it_companies)
