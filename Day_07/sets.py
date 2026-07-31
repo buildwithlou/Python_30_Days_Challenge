324b69b99cc637d3c539050092c05e9740ae1d2d
buildwithlou <buildwithlou@gmail.com>
2026-07-30 21:52:02 -0400

    Day 07 - Exercises (1)

---
+++ Day_07/sets.py
@@ -0,0 +1,8 @@
+# Exercises - Day 07
+# sets
+it_companies = {"Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+# Finding the length of the it_companies
+print("The length of the it_companies set is: ", len(it_companies))
