81d198c1b9881b0a3519b6587539c4f6579f8bfe
buildwithlou <buildwithlou@gmail.com>
2026-08-02 21:11:39 -0400

    Day 08 - Exercises (2)

--- Day_08/dictionaries.py
+++ Day_08/dictionaries.py
@@ -1,2 +1,9 @@
 # Creating an empty dictionary called dog
 dog = {}
+# Adding items to the dictionary
+dog["name"] = "Francisco"
+dog["color"] = "Chocolate dapple"
+dog["breed"] = "Dachshund"
+dog["legs"] = 4
+dog["age"] = "4 weeks"
+print(dog)
