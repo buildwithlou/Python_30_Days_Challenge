e0506bd55872aaf15971d0e2bfb9a164a24d3e6c
buildwithlou <buildwithlou@gmail.com>
2026-08-02 21:15:02 -0400

    Day 08 - Exercises (3)

--- Day_08/dictionaries.py
+++ Day_08/dictionaries.py
@@ -7,3 +7,15 @@ dog["breed"] = "Dachshund"
 dog["legs"] = 4
 dog["age"] = "4 weeks"
 print(dog)
+# Creting another dictionary called student
+student = {
+    "first_name": "William",
+    "last_name": "Suarez",
+    "gender": "Men",
+    "age": "29",
+    "marital status": "Married",
+    "skills": ["Bookepping", "Accountant", "Finance"],
+    "country": "USA",
+    "city": "Miami Lakes",
+    "address": "15525 Miami Lakeway N",
+}
