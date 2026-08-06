b3a8d5fee5245e0230bccaa8aca16ceeeaf94ebb
buildwithlou <buildwithlou@gmail.com>
2026-08-05 22:39:26 -0400

    Day 09 - Exercises (2)

--- Day_09/conditionals.py
+++ Day_09/conditionals.py
@@ -5,3 +5,20 @@ if age > 18 or age == 18:
     print("You are old enough to drive.")
 else:
     print("You need ", 18 - age, " more years to learn to drive.")
+# Comparing the vales of my_age and your_age using if ... else
+my_age = 25
+your_age = int(input("Enter your age: "))
+if my_age > your_age:
+    diff = my_age - your_age
+    if diff == 1:
+        print("I am 1 year older than you.")
+    else:
+        print("I am ", diff, " years older than you.")
+elif my_age < your_age:
+    diff = your_age - my_age
+    if diff == 1:
+        print("You are 1 year older than me.")
+    else:
+        print("You are ", diff, " years older than me.")
+else:
+    print("We are the same age.")
