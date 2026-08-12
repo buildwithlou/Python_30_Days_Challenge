3503a929d586d7b35327f7c903276f8515a11d0a
buildwithlou <buildwithlou@gmail.com>
2026-08-12 17:19:25 -0400

    Day 10 - Loops

--- Day10.py
+++ Day10.py
@@ -41,3 +41,34 @@ for company in it_companies:
 
 # Break and continue; we use it when we want to stop our loop before it is completed
 numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print("Next number should be ", number + 1) if number != 5 else print("loop's end")
+print("outside the loop")
+# The range function
+lst = list(range(11))
+print(lst)
+st = set(range(1, 11))
+print(st)
+for number in range(11):
+    print(number)
+# Nested for Loop
+person = {
+    "first_name": "Asabeneh",
+    "last_name": "Yetayeh",
+    "age": 250,
+    "country": "Finland",
+    "is_marred": True,
+    "skills": ["JavaScript", "React", "Node", "MongoDB", "Python"],
+    "address": {"street": "Space street", "zipcode": "02210"},
+}
+for key in person:
+    if key == "skills":
+        for skill in person["skills"]:
+            print(skill)
+# For Else
+for number in range(11):
+    print(number)
+print("The loop stops at", number)
