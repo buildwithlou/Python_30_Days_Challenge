e3ddd3cb2d71bee9d50c18c03f70a5aa9244787b
buildwithlou <buildwithlou@gmail.com>
2026-08-17 23:53:44 -0400

    Day 10 - Exercises (9)

--- Day_10/loops.py
+++ Day_10/loops.py
@@ -30,3 +30,8 @@ for i in range(101):
 for i in range(101):
     if i % 2 != 0:
         print(i)
+# Using for loop to iterate from 0 to 100 and print the sum of all numbers
+sum = 0
+for i in range(101):
+    sum += i
+print(f"The sum of all numbers is: {sum}")
