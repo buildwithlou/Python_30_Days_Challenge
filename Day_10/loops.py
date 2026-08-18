ddc5e908e40edfd12d536638f93c493f5e9a344a
buildwithlou <buildwithlou@gmail.com>
2026-08-17 23:57:03 -0400

    Day 10 - Exercises (10)

--- Day_10/loops.py
+++ Day_10/loops.py
@@ -35,3 +35,12 @@ sum = 0
 for i in range(101):
     sum += i
 print(f"The sum of all numbers is: {sum}")
+# Using for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+sum_evens = 0
+sum_odds = 0
+for i in range(101):
+    if i % 2 == 0:
+        sum_evens += i
+    else:
+        sum_odds += i
+print(f"The sum of all evens is {sum_evens}. And the sum of all odds is {sum_odds}.")
