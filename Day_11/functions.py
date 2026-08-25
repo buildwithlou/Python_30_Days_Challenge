68462d3cdc095aa9f5862ab0de301f38fa301a1c
buildwithlou <buildwithlou@gmail.com>
2026-08-24 23:24:12 -0400

    Day 11 - Exercises (2)

--- Day_11/functions.py
+++ Day_11/functions.py
@@ -5,3 +5,14 @@ def add_two_numbers(num1, num2):
 
 
 print("Taking two parameters and returning the sum: ", add_two_numbers(5, 10))
+
+print(50 * "#")
+
+
+def circle_area(radius, pi=3.14):
+    area = pi * radius**2
+    return area
+
+
+print("Calculating the area of a circle: ", circle_area(5))
+print(50 * "#")
