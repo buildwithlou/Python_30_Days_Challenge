0b0bc851b95b06341b23ddf4d376493f7073f008
buildwithlou <buildwithlou@gmail.com>
2026-09-08 22:45:53 -0400

    Updates - Day 11

--- Day_11/functions.py
+++ Day_11/functions.py
@@ -237,6 +237,8 @@ def calculate_median(lst):
         middle2 = nums[n // 2]
         return (middle1 + middle2) / 2
 
+def calculate_mode(lst):
 
-print("Calculating mean", calculate_mean([10, 20, 30, 40, 50]))
-print("Calculating mean", calculate_median([20, 10, 30, 40, 50, 60]))
+
+# print("Calculating mean", calculate_mean([10, 20, 30, 40, 50]))
+# print("Calculating mean", calculate_median([20, 10, 30, 40, 50, 60]))
