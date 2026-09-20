44bebc1c86bdb50b95543d7e74525c26a6d9446d
buildwithlou <buildwithlou@gmail.com>
2026-09-19 23:39:33 -0400

    Updates - Day 12

---
+++ Day12.py
@@ -0,0 +1,4 @@
+# Modules - Day 12
+# Creating a module here to be imported in main.py file
+def generate_full_name(firstname, lastname):
+    return firstname + " " + lastname
---
+++ main.py
@@ -0,0 +1,7 @@
+import Day12
+
+# importing the function from Day12.py file and calling it in main.py file
+print(Day12.generate_full_name("Lou", "Pampa"))
+
+# importing all the functions in case we had
+###from Day12 import *
